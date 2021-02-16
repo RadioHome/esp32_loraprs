2ce63cc40ba6d33e9444d5ff6ca1f1a9c2b7bb3f
TomF8COD <tom.moretto@gmail.com>
2021-02-16 08:24:35 +0100

    fix Oled constructor

--- display.cpp
+++ display.cpp
@@ -7,10 +7,10 @@
 #include <U8g2lib.h>
 
 #include "display.h"
-
 //Oled 
-U8G2_SH1106_128X64_NONAME_F_HW_I2C display(U8G2_R0);
-//U8G2_SSD1306_128X64_NONAME_2_SW_I2C display (U8G2_R0, OLED_SCL, OLED_SDA);
+//U8G2_SH1106_128X64_NONAME_F_HW_I2C display(U8G2_R0);
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C display (U8G2_R0, OLED_RESET);
+
 
 void setup_display()
 {
@@ -20,6 +20,7 @@ void setup_display()
 	delay(20);
 	digitalWrite(OLED_RESET, HIGH);
 #endif
+
   Serial.println("Setup screen...");
 	Wire.begin(OLED_SDA, OLED_SCL);
 	if(!display.begin())
@@ -38,15 +39,11 @@ void clear_display()
 void show_display(String header, String line1, String line2, int wait)
 {
 	display.clearBuffer();
-  display.setFont(u8g2_font_10x20_tf);
-  display.setCursor(0,16);
-  display.println(header);
-  display.setFont(u8g2_font_8x13_tf);
-  display.setCursor(0,32);
-  display.println(line1);
-  display.setCursor(0,48);
-  display.println(line2);
-
+	display.setFont(u8g2_font_ncenB10_tf);
+  display.drawStr(0, 16, header.c_str());
+  display.setFont(u8g2_font_ncenB08_tf);
+  display.drawStr(0, 32, line1.c_str());
+  display.drawStr(0, 48, line2.c_str());
   display.sendBuffer();
   delay(wait);
 }
